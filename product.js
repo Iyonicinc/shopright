7b6a7488d4984c71c28d05a1ba811e2461e22805
ian karani <iyoniccollections@gmail.com>
2024-10-25 16:08:36 +0300

    update content

---
+++ product.js
@@ -0,0 +1,34 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    1: {
+      name : "NEW HP Laptop 15s Intel Core i5 FHD",
+      img : "img/NEW HP Laptop 15s.png",
+      price : 74500
+    },
+    2: {
+      name : "HP EliteBook x360 1030 G4 Intel Core i7 8th Gen",
+      img : "img/HP EliteBook x360.jpg",
+      price : 74500
+    },
+    3: {
+      name : "HP Elite Dragonfly Core i5 8th Gen",
+      img : "img/HP Elite Dragonfly Core i5 8th Gen.jpg",
+      price : 74500
+    },
+    4: {
+      name : "Dell Latitude 3410 Core i5 10th Gen",
+      img : "img/Dell Latitude 3410 Core.jpeg",
+      price: 33999
+    },
+    5: {
+      name : "Dell Latitude 5410 Core i5 10th Gen",
+      img : "img/Latitude-5410.jpeg",
+      price : 34999  
+    },
+    6: {
+      name : "Dell Latitude 5490 Core i5 FHD Refurbished",
+      img : "img/Dell-Latitude-5490-Business-Laptop.png",
+      price : 27999  
+    },
+
+    }
