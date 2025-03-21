f6d8b0213bcb4b97623c070b6116cbe11ca5fb48
1234567890 <karanieyan9@gmail.com>
2025-03-21 14:11:32 +0300

    dead

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://shopright.iyonicorp.com' }));
 app.use(bodyParser.json()); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
